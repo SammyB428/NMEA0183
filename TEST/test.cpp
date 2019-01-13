d96e67b1cedba6ee3f9cbffd4fc498da3a94fce0
Sam Blackburn <sam_blackburn@pobox.com>
2019-01-13 08:36:42 -0500

    Got the test harness updated.
    Kinda forgot that one.

--- TEST/test.cpp
+++ TEST/test.cpp
@@ -7,7 +7,7 @@ Dr. Richard Garwin
 
 The MIT License (MIT)
 
-Copyright (c) 1996-2015 Sam Blackburn
+Copyright (c) 1996-2019 Sam Blackburn
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -28,10 +28,14 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-#include "nmea0183.h"
+#include "../nmea0183.h"
 #pragma hdrstop
 
-#pragma comment( lib, "nmea0183.lib" )
+#if defined( _DEBUG )
+#pragma comment( lib, "../x64/Debug/nmea0183.lib" )
+#else
+#pragma comment( lib, "../x64/Release/nmea0183.lib" )
+#endif
 
 /*
 ** This is the test scenario. Add more sentences to the list to test them
@@ -53,18 +57,18 @@ public:
             std::string sentence;
 
             nmea0183.GetSentence(sentence);
-            printf("\nCan't parse \"%s\"\n", (const char *)sentence.c_str());
-            printf("Failed with \"%s\"\n", (const char *)nmea0183.ErrorMessage.c_str());
-            printf("Last Sentence Received is \"%s\"\n", (const char *)nmea0183.LastSentenceIDReceived.c_str());
-            printf("Last Sentence Parsed is   \"%s\"\n\n", (const char *)nmea0183.LastSentenceIDParsed.c_str());
+            printf("\nCan't parse \"%s\"\n", sentence.c_str());
+            printf("Failed with \"%s\"\n", nmea0183.ErrorMessage.c_str());
+            printf("Last Sentence Received is \"%s\"\n", nmea0183.LastSentenceIDReceived.c_str());
+            printf("Last Sentence Parsed is   \"%s\"\n\n", nmea0183.LastSentenceIDParsed.c_str());
         }
         else
         {
-            printf("%s parsed OK from %s\n", (const char *)nmea0183.LastSentenceIDParsed.c_str(), (const char *)nmea0183.ExpandedTalkerID.c_str());
+            printf("%s parsed OK from %s\n", nmea0183.LastSentenceIDParsed.c_str(), nmea0183.ExpandedTalkerID.c_str());
 
             if (nmea0183.PlainText.length() != 0)
             {
-                printf("In plain English, %s\n", (const char *)nmea0183.PlainText.c_str());
+                printf("In plain English, %s\n", nmea0183.PlainText.c_str());
             }
         }
     }
@@ -104,7 +108,7 @@ int main()
    test_sentences.push_back( "$LCRMA,A,4226.26,N,07125.89,W,14182.3,26026.7,8.5,275.,14.0,W*68\r\n" ); // NMEA0183 document, pg 40
    test_sentences.push_back( "$LCRMA,V,4226.26,N,07125.89,W,14182.3,26026.7,8.5,275.,14.0,W*7F\r\n" ); // NMEA0183 document, pg 40
    test_sentences.push_back( "$LCRMA,A,4226.265,N,07125.890,W,14172.33,26026.71,8.53,275.,14.0,W*53\r\n" ); // NMEA0183 document, pg 40
-   test_sentences.push_back( "$GPRMB,A,0.66,L,003,004,4917.24,N,12309.57,W,001.3,052.5,000.5,V*0B\r\n" ); // Peter Bennett, bennett@triumf.ca
+   test_sentences.push_back( "$GPRMB,A,0.66,L,003,004,4917.24,N,12309.57,W,001.3,052.5,000.5,V*20\r\n" ); // Peter Bennett, bennett@triumf.ca
    test_sentences.push_back( "$GPRMB,A,4.08,L,EGLL,EGLM,5130.02,N,00046.34,W,004.6,213.9,122.9,A*3D\r\n" ); // pwyld@cix.compulink.co.uk ("Peter Wyld")
    test_sentences.push_back( "$GPRMC,225446,A,4916.45,N,12311.12,W,000.5,054.7,191194,020.3,E*68\r\n" ); // Peter Bennett, bennett@triumf.ca
    test_sentences.push_back( "$GPRMC,220516,A,5133.82,N,00042.24,W,173.8,231.8,130694,004.2,W*70\r\n" ); // pwyld@cix.compulink.co.uk ("Peter Wyld")
@@ -115,17 +119,19 @@ int main()
    test_sentences.push_back( "$GPWPL,5128.62,N,00027.58,W,EGLL*59\r\n" ); // pwyld@cix.compulink.co.uk ("Peter Wyld")
    test_sentences.push_back( "$GPXTE,A,A,0.67,L,N*6F\r\n" ); // Peter Bennett, bennett@triumf.ca
    test_sentences.push_back( "$GPXTE,A,A,4.07,L,N*6D\r\n" ); // pwyld@cix.compulink.co.uk ("Peter Wyld")
-#endif
-   test_sentences.push_back( "$GPGGA,103050,3912.073,N,07646.887,W,1,08,1.8,2.5,M,-34.0,M,,*72\r\n" ); // Me sitting at home with my Garmin 12XL
-   
+   test_sentences.push_back("$GPGGA,103050,3912.073,N,07646.887,W,1,08,1.8,2.5,M,-34.0,M,,*72\r\n"); // Me sitting at home with my Garmin 12XL
+
    /*
    ** Finish up with some proprietary sentences, just for giggles
    */
 
-   test_sentences.push_back( "$PGRMZ,93,f,3*21\r\n" ); // Peter Bennett, bennett@triumf.ca
-   test_sentences.push_back( "$PGRMZ,201,f,3*18\r\n" ); // pwyld@cix.compulink.co.uk ("Peter Wyld")
-   test_sentences.push_back( "$PSLIB,,,J*22\r\n" ); // Peter Bennett, bennett@triumf.ca
-   test_sentences.push_back( "$PSRDA003[470738][1224523]??RST47,3809,A004*47\r\n" ); // NMEA0183 document, pg 40
+   test_sentences.push_back("$PGRMZ,93,f,3*21\r\n"); // Peter Bennett, bennett@triumf.ca
+   test_sentences.push_back("$PGRMZ,201,f,3*18\r\n"); // pwyld@cix.compulink.co.uk ("Peter Wyld")
+   test_sentences.push_back("$PSLIB,,,J*22\r\n"); // Peter Bennett, bennett@triumf.ca
+   test_sentences.push_back("$PSRDA003[470738][1224523]??RST47,3809,A004*47\r\n"); // NMEA0183 document, pg 40
+#else
+   test_sentences.push_back("$GPRMB,A,0.66,L,003,004,4917.24,N,12309.57,W,001.3,052.5,000.5,V*0B\r\n"); // Peter Bennett, bennett@triumf.ca
+#endif
 
    /*
    ** Now loop through the list and attempt to parse each sentence
